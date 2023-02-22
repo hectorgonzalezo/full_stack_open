e8ed902e4bf2d46cb5ce09d31c397f14e3b69bcd
Hector Gonzalez <gorozco.hector@gmail.com>
2023-02-22 11:21:49 -0600

    Fix part1 directory structure

---
+++ part1/courseInfo/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  const friends = [ 'Peter', 'Maya']
+
+  return (
+    <div>
+      <p>{friends}</p>
+    </div>
+  )
+}
+
+
+export default App;
